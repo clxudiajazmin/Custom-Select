3fceece2d684005aad84718a8e6d5b095af64bc9
Claudia Jazmín Soria Saavedra <clxudiajazmin@gmail.com>
2021-03-02 17:03:13 +0100

    Basic configuration

--- select.js
+++ select.js
@@ -17,6 +17,7 @@ export default class Select{
         this.optionCustomElement = document.createElement('ul')
 
         setupCustomElement(this)
+        element.style.display = "none"
 
         element.after(this.customElement)
     }
@@ -24,11 +25,26 @@ export default class Select{
     get selectedOption(){
         return this.options.find(option => option.selected)
     }
+
+    selectValue(value) {
+        const newSelectedOption = this.options.find(option => {
+            return option.value === value
+          })
+          const prevSelectedOption = this.selectedOption
+          prevSelectedOption.selected = false
+          prevSelectedOption.element.selected = false
+      
+          newSelectedOption.selected = true
+          newSelectedOption.element.selected = true
+      
+          this.labelElement.innerText = newSelectedOption.label
+    }
 }
     
 function setupCustomElement(select){
     //Adding new class for not changing style
     select.customElement.classList.add('custom-select-container')
+    select.customElement.tabIndex = 0
 
     select.labelElement.classList.add('custom-select-value')
     select.labelElement.innerText = select.selectedOption.label
@@ -40,14 +56,30 @@ function setupCustomElement(select){
 
     select.options.forEach(option => {
         const optionElement = document.createElement('li')
-        optionElement.classList.add('custon-select-option')
+        optionElement.classList.add('custom-select-option')
         optionElement.classList.toggle('selected', option.selected)
         optionElement.innerText = option.label
         optionElement.dataset.value = option.value
+        optionElement.addEventListener('click', () => {
+            select.optionCustomElement.
+                querySelector(`[data-value = "${select.selectedOption.value}"]`)
+                .classList.remove('selected')
+            select.selectValue(option.value)
+            optionElement.classList.add('selected')
+            select.optionCustomElement.classList.remove('show')
+        })
         select.optionCustomElement.append(optionElement)
     })
     //Assign options to element
     select.customElement.append(select.optionCustomElement)
+
+    select.labelElement.addEventListener('click', () =>{
+        select.optionCustomElement.classList.toggle("show")
+    })
+
+    select.customElement.addEventListener('blur', () => {
+        select.optionCustomElement.classList.remove("show")
+    })
 }
 
 function getFormattedOptions(optionElements){
